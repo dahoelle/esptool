7034940b86cf7ea36d719126c933fe8f8fb180fc
dahoelle <hoelledavid@gmail.com>
2025-07-02 12:00:51 +0200

    Create boilerplate.ino

---
+++ boilerplate.ino
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+#include "boilerplate.h"
+#include <vector>
+
+std::vector<ISetup*> setupables;
+
+void setup() {
+    Serial.begin(115200);
+    delay(1000);
+
+    // Create and collect ISetup instances
+    setupables.push_back(new DummySetup("GPIO"));
+    setupables.push_back(new DummySetup("Sensors"));
+    setupables.push_back(new MyWebServer());
+    setupables.push_back(new DummySetup("Scheduler"));
+
+    Serial.println("Beginning setup sequence...");
+
+    for (size_t i = 0; i < setupables.size(); ++i) {
+        Serial.printf("Setup step %zu/%zu starting\n", i + 1, setupables.size());
+        setupables[i]->setup();
+        Serial.printf("Setup step %zu/%zu completed\n", i + 1, setupables.size());
+    }
+
+    Serial.println("All setup steps complete");
+}
+
+void loop() {
+    delay(1000);
+}
